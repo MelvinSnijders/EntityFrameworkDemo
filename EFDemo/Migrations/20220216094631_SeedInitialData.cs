72199f2577ae829bb17911c6a0354bb658d846d7
Melvin Snijders <mail@melvinsnijders.nl>
2022-02-16 10:55:02 +0100

    Added seeding

---
+++ EFDemo/Migrations/20220216094631_SeedInitialData.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDemo.Migrations
+{
+    public partial class SeedInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Course",
+                columns: new[] { "Id", "Code", "Naam" },
+                values: new object[,]
+                {
+                    { 1, "DW", "Discrete Wiskunde" },
+                    { 2, "GP", "Games Programming" },
+                    { 3, "ANA1", "Analyse 1" },
+                    { 4, "ANA2", "Analyse 2" },
+                    { 5, "C#", "C-sharp" },
+                    { 6, "GESP", "Gesprekken in Organisaties" },
+                    { 7, "WP", "Web programmeren" },
+                    { 8, "PYT", "Python" },
+                    { 9, "JAV", "Java programmeren" },
+                    { 10, "ISEC", "Inleiding Security" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
